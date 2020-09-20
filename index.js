d2fef6608b0e5247f7901fbce51fd39f0840bc4c
GagoAnimate <65516039+gagominecraft12@users.noreply.github.com>
2020-09-19 22:35:11 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${751258813491052587}!`);
+  console.log(`Logged in as ${NzQyNDU4MjE1MjA3NDAzNjYx.XzGaGA.KM64K1sJwyVZmXXHG93_z_hcZDU}!`);
 });
 
 mnclient.on('message', msg => {
-    if (msg.content === 'ping') {
-      msg.reply('Pong!');
+    if (msg.content === 'help') {
+      msg.reply('Want To Help?, DM Me For Help');
   }
 });
 
